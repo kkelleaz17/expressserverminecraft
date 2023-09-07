fedc1ba80dc3076e29cdef37f976ef5381c91c12
kkelleaz17 <111470082+kkelleaz17@users.noreply.github.com>
2023-09-07 12:52:50 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/images'));
+
+const Mobs = require('./Mobs.json');
+
+app.set('view engine', 'ejs');
+app.set('views', './View');
+
+// Use bodyParser.json() for parsing JSON data for all requests
+app.use(bodyParser.json());
+
+// Use bodyParser.urlencoded() for parsing URL-encoded data for all requests
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('mobs', { Mobs: Mobs });
+});
+
+app.get('/Mobs', (req, res) => {
+    // You can access query parameters directly for GET requests
+    const searchName = String(req.query.Name).toLowerCase();
+    console.log(searchName)
+    if (!searchName) {
+        res.redirect('/');
+    } else {
+        const Mob = Mobs.filter(e => e.name.toLowerCase() === searchName);
+        console.log(Mob)
+        if(Mob === []){
+            res.redirect('/');
+        }
+        res.render('mobs', { Mobs: Mob });
+    }
+});
+
+// app.get('/Mobs/*', (req, res))
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running right now on: http://localhost:3000');
+});
+
+  
